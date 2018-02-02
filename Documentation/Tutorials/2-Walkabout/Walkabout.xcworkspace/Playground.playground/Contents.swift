9872b2e98852b61b402fb9b04d8ff3792575dee0
Kenny Dang <kdang1337@gmail.com>
2018-02-02 13:04:38 -0800

    Removed unnecessary code

--- Documentation/Tutorials/2-Walkabout/Walkabout.xcworkspace/Playground.playground/Contents.swift
+++ Documentation/Tutorials/2-Walkabout/Walkabout.xcworkspace/Playground.playground/Contents.swift
@@ -1,11 +1,2 @@
 import Foundation
 import PlaygroundSupport
-
-var myCoolString = "What a cool string"
-
-let someIndex = 2
-
-let index = myCoolString.index(myCoolString.startIndex, offsetBy: someIndex + 2)
-let substring = myCoolString[..<index]
-
-
